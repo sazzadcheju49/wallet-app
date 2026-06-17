a8173e9529d4ad5708bee44f26ca2fb009d06470
sazzadcheju49 <110609478+sazzadcheju49@users.noreply.github.com>
2026-06-17 19:42:00 +0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = 'wallet-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+});
